9401ba626807204316b13b84f4404439920e3d60
23021612-bit <23021612@vnu.edu.vn>
2026-09-15 14:49:19 +0700

    fix

--- api_session_01/app1.py
+++ api_session_01/app1.py
@@ -5,8 +5,8 @@ def health():
     return jsonify({"status": "ok"}), 200
 @app.route("/echo", methods=["POST"])
 def echo():
-    data = request.get.json(silent=True) or {}
-    return jsonify({"you sent": data}), 200
+    data = request.get_json(silent=True) or {}
+    return jsonify({"you_sent": data}), 200
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug= True)
