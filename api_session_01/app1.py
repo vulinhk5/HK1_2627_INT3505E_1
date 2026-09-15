ea38a6a5e81d8eabf203e912cfabb3c34181ce1a
23021612-bit <23021612@vnu.edu.vn>
2026-09-15 15:10:58 +0700

    fix

--- api_session_01/app1.py
+++ api_session_01/app1.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+
 @app.route("/health", methods=["GET"])
 def health():
-    return jsonify({"status": "ok"}), 200
+  return jsonify({"status" : "ok"}), 200
+
 @app.route("/echo", methods=["POST"])
 def echo():
-    data = request.get_json(silent=True) or {}
-    return jsonify({"you_sent": data}), 200
+  data = request.get_json(silent=True) or {}
+  return jsonify({"you_sent" : data}), 200
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug= True)
-
+    app.run(host="127.0.0.1", port=5000, debug=True)
 
